dfc6966a0b1d742af3046649b1117590eb078147
Erick Zanardo <erickzanardoo@gmail.com>
2020-06-27 23:53:52 -0300

    Running on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/flame.dart';
 import './flame_and_watch.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Flame.util.fullScreen();
-  await Flame.util.setLandscape();
+  if (!kIsWeb) {
+    await Flame.util.fullScreen();
+    await Flame.util.setLandscape();
+  }
   runApp(FlameAndWatchScreen());
 }
